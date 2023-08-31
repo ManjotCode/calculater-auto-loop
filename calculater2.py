0519759b3aca4e64ac6c7750e6f5e5bec9b43c34
ManjotCode <142479289+ManjotCode@users.noreply.github.com>
2023-08-31 10:59:38 +0530

    Add files via upload

---
+++ calculater2.py
@@ -0,0 +1,8 @@
+from math import pi,sqrt
+calculate=True
+print("Division /","Floor Division //","Multiplication *" ,"Expoention **", "Remainder %", "Addition +", "Subtraction -",sep='\n')
+while calculate:
+    print("Use pi as pi","use Square root as sqrt()", sep='\n')
+    #getint input
+    inp=eval(input("Write your equation: "))
+    print(inp)
